9487411dad4fe8012f5f28f39d042118557d1c82
melcalixte8 <melissacalixte@ufl.edu>
2024-10-21 15:21:41 -0400

    update

--- lab6.py
+++ lab6.py
@@ -1,11 +1,5 @@
 # Melissa Calixte
-print ('Menu')
-print('-------------')
-print('1. Encode\n2. Decode\n3. Quit')
-
-option = int(input('Please enter an option: '))
-if option == 1:
-    password = input('Please enter your passcode to encode: ')
+def encoder(password):
     string = ""
     for i in password:
         i = int(i)
@@ -14,4 +8,17 @@ if option == 1:
             i = int(i - 10)
         string += str(i)
     print(string)
+
+if __name__ == '__main__':
+
+    print('Menu')
+    print('-------------')
+    print('1. Encode\n2. Decode\n3. Quit')
+
+    option = int(input('Please enter an option: '))
+    if option == 1:
+        password = input('Please enter your passcode to encode: ')
+        encoder(password)
+
+
 
