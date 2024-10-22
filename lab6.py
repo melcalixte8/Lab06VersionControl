ca73eb6119e36b42f27e7ab95632d443f6be58de
melcalixte8 <melissacalixte@ufl.edu>
2024-10-22 13:55:24 -0400

    added the decode function

--- lab6.py
+++ lab6.py
@@ -7,7 +7,7 @@ def encoder(password):
         if i >= 10:
             i = int(i - 10)
         string += str(i)
-    print(string)
+    return string
 
 if __name__ == '__main__':
 
@@ -16,9 +16,17 @@ if __name__ == '__main__':
     print('1. Encode\n2. Decode\n3. Quit')
 
     option = int(input('Please enter an option: '))
+
     if option == 1:
         password = input('Please enter your passcode to encode: ')
-        encoder(password)
+        password = encoder(password)
+        print("Your password has been encoded and stored!")
+
+    elif option == 2:
+        pass
+
+
+
 
 
 
