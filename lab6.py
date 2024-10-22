9b84d5730eba1bf2e8264da6861f8c4152801e1a
melcalixte8 <melissacalixte@ufl.edu>
2024-10-22 14:03:39 -0400

    made some changes

--- lab6.py
+++ lab6.py
@@ -10,20 +10,25 @@ def encoder(password):
     return string
 
 if __name__ == '__main__':
+    while True:
+        print('Menu')
+        print('-------------')
+        print('1. Encode\n2. Decode\n3. Quit')
 
-    print('Menu')
-    print('-------------')
-    print('1. Encode\n2. Decode\n3. Quit')
+        option = int(input('Please enter an option: '))
 
-    option = int(input('Please enter an option: '))
+        if option == 1:
+            password = input('Please enter your passcode to encode: ')
+            password = encoder(password)
+            print("Your password has been encoded and stored!")
 
-    if option == 1:
-        password = input('Please enter your passcode to encode: ')
-        password = encoder(password)
-        print("Your password has been encoded and stored!")
+        elif option == 2:
+            pass
 
-    elif option == 2:
-        pass
+        elif option == 3:
+            break
+
+
 
 
 
